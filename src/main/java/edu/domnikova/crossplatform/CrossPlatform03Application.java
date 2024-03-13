d1df3d5f986bf90a058926536a7db2e6ed59af4b
chntk <dddomnikova@gmail.com>
2024-03-14 00:34:00 +0300

    Init commit

---
+++ src/main/java/edu/domnikova/crossplatform/CrossPlatform03Application.java
@@ -0,0 +1,13 @@
+package edu.domnikova.crossplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatform03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPlatform03Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/domnikova/crossplatform/CrossPlatform03ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.domnikova.crossplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatform03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
