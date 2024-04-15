6b240b6940fd5f46748d15f53383dfd0d72a52ef
chntk <dddomnikova@gmail.com>
2024-04-15 21:43:49 +0300

    4.4 Made menu icons parameterized

---
+++ src/main/java/edu/domnikova/crossplatform/CrossPlatform03Configuration.java
@@ -0,0 +1,19 @@
+package edu.domnikova.crossplatform;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class CrossPlatform03Configuration {
+
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+        return resolver;
+    }
+}
